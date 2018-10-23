28cb40b46225f0d6f658d62470e7a5a0a58ca102
wulianyi1998 <wulianyi1998@163.com>
2018-10-23 16:37:41 +0800

    lab3

--- lock_smain.cc
+++ lock_smain.cc
@@ -45,9 +45,9 @@ main(int argc, char *argv[])
   server.reg(lock_protocol::stat, &ls, &lock_server_cache::stat);
   server.reg(lock_protocol::release, &ls, &lock_server_cache::release);
   server.reg(lock_protocol::acquire, &ls, &lock_server_cache::acquire);
-=======
+
   server.reg(lock_protocol::release, &ls, &lock_server::release);
->>>>>>> lab2
+
 #endif
 
 
