b814cc93d4b5a2654d83cfa7984f881f31702268
wulianyi1998 <wulianyi1998@163.com>
2018-10-31 06:17:16 +0000

    temp

--- lock_smain.cc
+++ lock_smain.cc
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-//#include "lock_server.h"
-#include "lock_server_cache.h"
+#include "lock_server.h"
+//#include "lock_server_cache.h"
 
 #include "jsl_log.h"
 
@@ -33,20 +33,20 @@ main(int argc, char *argv[])
   //jsl_set_debug(2);
 
 #ifndef RSM
-  /*lock_server ls;
+  lock_server ls;
   rpcs server(atoi(argv[1]), count);
   server.reg(lock_protocol::stat, &ls, &lock_server::stat);
   server.reg(lock_protocol::acquire, &ls, &lock_server::acquire);
-<<<<<<< HEAD
-  server.reg(lock_protocol::release, &ls, &lock_server::release);*/
+//<<<<<<< HEAD
+  server.reg(lock_protocol::release, &ls, &lock_server::release);
 
-  lock_server_cache ls;
+  /*lock_server_cache ls;
   rpcs server(atoi(argv[1]), count);
   server.reg(lock_protocol::stat, &ls, &lock_server_cache::stat);
   server.reg(lock_protocol::release, &ls, &lock_server_cache::release);
-  server.reg(lock_protocol::acquire, &ls, &lock_server_cache::acquire);
+  server.reg(lock_protocol::acquire, &ls, &lock_server_cache::acquire);*/
 
-  server.reg(lock_protocol::release, &ls, &lock_server::release);
+  //server.reg(lock_protocol::release, &ls, &lock_server::release);
 
 #endif
 
